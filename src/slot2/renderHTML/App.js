ffebeb13173737b7a088205af9662ffd137e166a
joe-jeo-ame-060717 <hoangvangiap060717@gmail.com>
2025-03-15 16:45:59 +0700

    thrid commit

---
+++ src/slot2/renderHTML/App.js
@@ -0,0 +1,17 @@
+import { Button } from "bootstrap";
+
+const App = () => {
+  return (
+    <div>
+      <button />
+      <code />
+      <input />
+      <label />
+      <p />
+      <select />
+      <table />
+      <ul />
+    </div>
+  );
+};
+export default App;
